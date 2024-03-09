2e1d06911cef0edee4aeca69bfda5608da680be3
Elmangobd <127646643+Elmangobd@users.noreply.github.com>
2024-03-08 22:55:33 -0600

    Update db.js
    mongodb+srv://20210648:uKCc4JJ0kvsy8n0F@cluster0.qrpmvnj.mongodb.net/?retryWrites=true&w=majority

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () =>{
     try {
-        mongoose.connect('mongodb://127.0.0.1:27017/Robopits');
+        mongoose.connect('mongodb+srv://20210648:uKCc4JJ0kvsy8n0F@cluster0.qrpmvnj.mongodb.net/?retryWrites=true&w=majority');
         console.log('===> La conexion se realizo correctamente');
     } catch (error) {
         console.log(error);
