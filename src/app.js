26927a966a8d02122df49800c59088e4fd007f3b
Elmangobd <127646643+Elmangobd@users.noreply.github.com>
2024-04-10 09:17:10 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 
 app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.robopits.online");
+  res.header("Access-Control-Allow-Origin", "https://www.robopits.online");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
