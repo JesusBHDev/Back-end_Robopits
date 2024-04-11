f08d0c04b9dd1f48c66838a49cc526feb9b7cca6
Ivan <jesus.bta.01@gmail.com>
2024-04-11 03:53:34 -0600

    imagen para productos

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -5,13 +5,14 @@ import { obtenerProductos,
          eliminarProducto,
          actualizarProducto
  } from "../controllers/Productos.controller.js";
+ import { upload } from "../config/multer.js";
 
 const router = Router();
 
 router.get('/Productos', obtenerProductos);
-router.post('/Productos', crearProducto);
+router.post('/Productos', upload.fields([{name: 'Imagen', maxCount: 1}]), crearProducto);
 router.get('/Producto/:id', obtenerProducto);
 router.delete('/Producto/:id', eliminarProducto);
-router.put('/Producto/:id', actualizarProducto);
+router.put('/Producto/:id',upload.fields([{name: 'Imagen', maxCount: 1}]), actualizarProducto);
 
 export default router;
