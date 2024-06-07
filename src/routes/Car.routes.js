8ad824e5bc482facc9d11919a545046ca1bcfe33
Jesús <jesus.bh.dev@gmail.com>
2024-06-07 13:21:22 -0600

    ya se puede ver el carrito del usuario

--- src/routes/Car.routes.js
+++ src/routes/Car.routes.js
@@ -1,8 +1,12 @@
-import express from 'express';
-import { agregarProductoAlCarrito } from '../controllers/Carrito.controller.js';
+import { Router } from 'express';
+import { agregarProductoAlCarrito, obtenerCarrito } from '../controllers/Carrito.controller.js';
 
-const router = express.Router();
+const router = Router();
 
+// Ruta para agregar producto al carrito
 router.post('/carritoagregar', agregarProductoAlCarrito);
+
+// Ruta para obtener el carrito del usuario
+router.get('/carrito/:userId', obtenerCarrito);
 
 export default router;
