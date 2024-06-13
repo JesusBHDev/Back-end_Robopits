0ce92431ca36f0106bd798d00d178caa6d7edd85
JesusBHDev <156105288+JesusBHDev@users.noreply.github.com>
2024-06-13 04:20:08 -0600

    Update auth.controller.js
    Prueba 6 para mantener la sesion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -79,16 +79,12 @@ export const login = async (req, res) => {
             },
             motivo: "Inicio de sesion"
         });
+        
+        res.cookie('token', token);
 
         // Guarda el registro de inicio de sesión
         await inicio.save();
         
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // Enviar solo sobre HTTPS en producción
-            sameSite: 'strict' // La cookie no se envía con solicitudes de origen cruzado
-        });
-
         // Envía la respuesta con los datos del usuario y el mensaje de inicio de sesión exitoso
         res.status(200).json({
             id: userFound._id,
