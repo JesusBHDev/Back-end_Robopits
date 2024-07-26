b4d41da09f85020aed57e22cc1e3a129dde96758
Jesús <jesus.bh.dev@gmail.com>
2024-07-26 09:25:06 -0600

    faltantes de recomendaciones

--- src/routes/Car.routes.js
+++ src/routes/Car.routes.js
@@ -4,19 +4,17 @@ import {
   obtenerCarrito,
   decrementarCantidadProducto,
   eliminarProductoDelCarrito,
-  incrementarCantidadProducto
+  incrementarCantidadProducto,
+  recomendaciones
 } from '../controllers/Carrito.controller.js';
 
 const router = Router();
 
-// Ruta para agregar producto al carrito
 router.post('/carritoagregar', agregarProductoAlCarrito);
-
-// Ruta para obtener el carrito del usuario
 router.get('/carrito/:userId', obtenerCarrito);
-
 router.post('/carrito/decrement', decrementarCantidadProducto);
 router.post('/carrito/remove', eliminarProductoDelCarrito);
 router.post('/carrito/increment', incrementarCantidadProducto);
+router.get('/recomendaciones', recomendaciones);
 
 export default router;
