8a1fe018639ec2ccacc6739ef4d21b7dfbb7fbbd
JesusBHDev <156105288+JesusBHDev@users.noreply.github.com>
2024-08-09 02:31:50 -0600

    Update Favoritos.model.js

--- src/models/Favoritos.model.js
+++ src/models/Favoritos.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const favoritoSchema = new Schema({
   userId: { 
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: 'User',
     required: true 
   },
