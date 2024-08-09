847539084d8bac72961502193d53602e4e102124
JesusBHDev <156105288+JesusBHDev@users.noreply.github.com>
2024-08-09 02:31:02 -0600

    Update Favoritos.model.js

--- src/models/Favoritos.model.js
+++ src/models/Favoritos.model.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const favoritoSchema = new Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  productos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Producto' }]
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true 
+  },
+  productos: [{ type: mongoose.Schema.Types.ObjectId,
+    ref: 'Producto'
+  }]
 });
 
 export default mongoose.model('Pedido', PedidoSchema);
