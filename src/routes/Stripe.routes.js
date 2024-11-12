79bf1e35dfec10840d33c6df52b22ba70b6d0e28
Angel0603 <20211050@uthh.edu.mx>
2024-11-12 01:35:20 -0600

    Se cambiaron las API Keys de Stripe

--- src/routes/Stripe.routes.js
+++ src/routes/Stripe.routes.js
@@ -6,7 +6,7 @@ dotenv.config();
 const router = Router();
 
 router.get('/stripe-key', (req, res) => {
-  res.json({ publishableKey: process.env.STRIPE_PUBLISHABLE_KEY });
+  res.json({ publishableKey: "pk_test_51QI5azD1XzE1fq2pk2QrNMWnltIDCCl8zswMuo5hFo6D0LRd6FnF26l1il0sH79G2bvIXrSpsnBgPrUvPtzSmluU00N8rpQQUq" });
 });
 
 router.post('/hoja-de-pago', createPaymentIntent);
