810f98ec3a3d3b90d99fba8c3e0a4477a6dd0ae2
Angel0603 <20211050@uthh.edu.mx>
2024-11-12 01:05:40 -0600

    Se agregó la configuracion correcta de Stripe

--- src/controllers/Stripe.controller.js
+++ src/controllers/Stripe.controller.js
@@ -1,17 +1,19 @@
 import Stripe from 'stripe';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export const hojaDePago = async (req, res) => {
+export const createPaymentIntent = async (req, res) => {
   try {
-    const amount = req.body.amount;
+    const { amount } = req.body;
     const customer = await stripe.customers.create();
     const ephemeralKey = await stripe.ephemeralKeys.create(
       { customer: customer.id },
       { apiVersion: '2024-10-28.acacia' }
     );
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
+      amount,
       currency: 'mxn',
       customer: customer.id,
       automatic_payment_methods: {
--- src/routes/Stripe.routes.js
+++ src/routes/Stripe.routes.js
@@ -1,12 +1,14 @@
-// En tu archivo de rutas (e.g., stripe.routes.js)
-import {Router} from 'express';
-import { hojaDePago } from '../controllers/Stripe.controller.js';
+import { Router } from 'express';
+import { createPaymentIntent } from '../controllers/Stripe.controller.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const router = Router();
 
 router.get('/stripe-key', (req, res) => {
   res.json({ publishableKey: process.env.STRIPE_PUBLISHABLE_KEY });
 });
 
-router.post('/hoja-de-pago', hojaDePago);
+router.post('/hoja-de-pago', createPaymentIntent);
 
 export default router;
